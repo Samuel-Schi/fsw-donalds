2afed1455edb586eda097d6879d2cddbbf5ca0f7
Samuel SFS <samucaggz@gmail.com>
2025-12-07 10:04:13 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+const ProductPAge =() => {
+    return (
+        <div>  className= ="p-5 border rounded shadow-md">
+            <h1 className="text-2xl font-bold mb-4">Products Page</h1>
+            <p>Welcome to the Products page!</p>
+            <button>Add Product</button>
+        </div>
+    );
+}
+   
