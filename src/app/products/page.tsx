11be2f7b88c6404d4d2b1f8e28d50c4e1009595e
Samuel SFS <samucaggz@gmail.com>
2026-01-18 10:17:00 -0300

    atualização projeto

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,14 +3,12 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6">Products Page</h1>
-      <div className="mb-4">
-        <Input placeholder="Search products..." className="w-full max-w-sm" />
-      </div>
-      <Button className="bg-blue-500 text-white hover:bg-blue-600">
-        Add New Product
-      </Button>
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="text-red-500">products page</h1>
+      <Button>FSW 7.0</Button>
+      <Input placeholder="Bora fechar esse projeto!" />
     </div>
   );
 };
+
+export default ProductPage;
